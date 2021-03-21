ed8d333f3e0dff61e0c912ef3a41d3e5c722f067
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-21 13:31:37 -0400

    Part 5, 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [ expanded, setExpanded ] = useState(false);
+
+  const expandedContent = () => {
+    return (
+      <div>
+        <p>{ blog.title } { blog.author } <button onClick={ () => setExpanded(false) }>{ expanded ? 'hide' : 'view' }</button></p>
+        <p>{ blog.url }</p>
+        <p>likes: { blog.likes } <button>like</button></p>
+      </div>
+    )
+  }
+
+  const minContent = () => {
+    return (
+      <div>
+        { blog.title } { blog.author } <button onClick={ () => setExpanded(true) }>view</button>
+      </div>
+    )
+  }
+
+  return (
+    <div class="blog">
+      { expanded ? expandedContent() : minContent() }
+    </div>
+  )
+}
 
 export default Blog
