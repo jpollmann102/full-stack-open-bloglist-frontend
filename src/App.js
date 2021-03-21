5b88fb06fb91e86ae489a8c65af50652a3d03536
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-21 12:17:17 -0400

    Part 5, 5.2 and 5.3

--- src/App.js
+++ src/App.js
@@ -8,11 +8,24 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )
+  }, []);
+
+  useEffect(() => {
+    const userJSON = window.localStorage.getItem('blog-user');
+    if(userJSON)
+    {
+      const user = JSON.parse(userJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
 
   const handleLogin = async (event) => {
@@ -23,19 +36,86 @@ const App = () => {
       setUser(userResponse);
       setUsername('');
       setPassword('');
+
+      window.localStorage.setItem('blog-user', JSON.stringify(userResponse));
+      blogService.setToken(userResponse.token);
     }catch(exception) {
+      console.log(exception);
       window.alert('Incorrect username or password');
     }
+  }
+
+  const createBlog = async (event) => {
+    event.preventDefault();
+
+    const newNote = {
+      title,
+      author,
+      url
+    };
+
+    try {
+      const createResponse = await blogService.createBlog(newNote);
+      setBlogs(blogs.concat(createResponse))
+    }catch(exception) {
+      console.log(exception);
+    }
+  }
+
+  const createBlogForm = () => {
+    return (
+      <div>
+        <h2>create new</h2>
+        <form onSubmit={ createBlog }>
+          <div>
+            title
+            <input
+              type="text"
+              value={ title }
+              name="Title"
+              onChange={ ({ target }) => setTitle(target.value) }
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="text"
+              value={ author }
+              name="Author"
+              onChange={ ({ target }) => setAuthor(target.value) }
+            />
+          </div>
+          <div>
+            url
+            <input
+              type="text"
+              value={ url }
+              name="Url"
+              onChange={ ({ target }) => setUrl(target.value) }
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
+    )
+  }
+
+  const handleLogout = () => {
+    setUser(null);
+    blogService.setToken('');
+    window.localStorage.removeItem('blog-user');
   }
 
   const loggedInContent = () => {
     return (
       <div>
         <h2>Hello, { user.name }</h2>
+        { true && createBlogForm() }
         <h2>blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
+        <button onClick={ handleLogout }>logout</button>
       </div>
     )
   }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,28 @@
 import axios from 'axios';
 const baseUrl = '/api/blogs';
 
+let token = null;
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`;
+}
+
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 }
 
-export default { getAll }
+const createBlog = async (blog) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.post(baseUrl, blog, config);
+  return response.data;
+}
+
+export default {
+  getAll,
+  createBlog,
+  setToken
+}
